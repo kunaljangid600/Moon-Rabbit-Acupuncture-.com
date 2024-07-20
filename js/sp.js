33d17240e1c6d643074251828c601384a2efc760
kunaljangid600 <kunaljangid600@gmail.com>
2024-07-20 22:05:00 +0530

    first commit

---
+++ js/sp.js
@@ -0,0 +1,10 @@
+// var parsts1 = document.querySelectorAll('.parsts1 p')
+// var megamenu = document.querySelector('#mega-menu')
+// var megamenu1 = document.querySelector('#mega-menu1')
+// console.log(parsts1)
+// parsts1.addEventListener(
+//     'mouseenter',
+//     function(){
+//         parsts1.classList.add('megamenu1')       
+//     }
+// )
